86f276474efa417e74065afa2ed30d44098101d8
Ronnie Rios <ronniesmail14@gmail.com>
2023-08-17 20:44:11 -0500

    tailwind style and added toggle feature

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import useToggle from "../hooks/useToggle";
+
+const Card = ({ data }) => {
+  const [isToggled, toggle] = useToggle(false);
+  const { title, body, id } = data;
+
+  return (
+    <div className="py-10 border border-gray-100 rounded-sm my-2">
+      <h1 className="text-2xl">
+        {id}: {title}
+      </h1>
+      {isToggled ? <p className="my-2">{body}</p> : ""}
+      <button onClick={(isToggled) => toggle(!isToggled)} className="p-2 bg-white text-black hover:scale-105 rounded-sm mt-4">
+        {isToggled ? "Hide Details" : "View Details"}
+      </button>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,9 @@
-
-import useToggle from "../hooks/useToggle";
 import useFetch from "../hooks/useFetch";
+import Card from "./Card";
 
 const URL = "https://jsonplaceholder.typicode.com/posts";
 
 const Display = () => {
-  const [isToggled, toggle] = useToggle(false);
   const { data, isLoading, error } = useFetch(URL);
 
   if (isLoading) {
@@ -16,25 +14,16 @@ const Display = () => {
     return <div>Error: {error.message}</div>;
   }
 
-  if (!isToggled) {
-    return (
-      <div>
-        <h1>Posts</h1>
-        <button onClick={() => toggle(true)}>Hide all posts</button>
-        <ul>
-          {data.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <button onClick={() => toggle(true)}>View</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post) => (
+          <Card key={post.id} data={post}/>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Display;
